12989f2ee08a0f131cb58174e53c65df6dd7c558
Paul Sinnett <paul.sinnett@gmail.com>
2021-03-16 15:31:24 +0000

    Initial commit

---
+++ Assets/ChangeColour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour : MonoBehaviour
+{
+    public Material unselected;
+    public Material selected;
+    Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void OnMouseDown()
+    {
+        renderer.material = selected;
+    }
+
+    void OnMouseUp()
+    {
+        renderer.material = unselected;
+    }
+}
